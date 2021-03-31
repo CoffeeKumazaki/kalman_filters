de91fc0572d1346db1b1cf6888700b4983f49b36
Kohei Kumazaki <kumazaki98@gmail.com>
2021-04-01 02:14:29 +0900

    use ekf

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 
 int main(int argc, char const *argv[]) {
 
-	SystemSimulator sim;
 	// state: x, y, yaw, v
 	Eigen::VectorXd x0(4);
 	// x0.setZero();
@@ -27,21 +26,36 @@ int main(int argc, char const *argv[]) {
 
 	R = R * R;
 	double dt = 1.0 / 30.0;
-	sim.init_model(x0, R, Q, dt);
+	std::shared_ptr<SystemModelA> model = std::make_shared<SystemModelA>(x0, R, Q, dt);
+	SystemSimulator sim(model);
+	sim.init();
+	Eigen::MatrixXd P(4, 4);
+	P << 1.0, 0.0, 0.0, 0.0,
+			 0.0, 1.0, 0.0, 0.0, 
+			 0.0, 0.0, 1.0, 0.0, 
+			 0.0, 0.0, 0.0, 1.0;
+	ExtendedKF ekf(model, P);
+	ekf.init(x0);
 	double t = 0;
 
 	// u: v, yaw
 	Eigen::VectorXd u(2);
 	u << 0.0, deg2rad(2.0);
+
+	std::vector<Eigen::VectorXd> res;
 	while (t < 60.0) {
 		sim.step(u);
+		ekf.predict(u);
+		ekf.update(sim.obs_results.back());
+		res.push_back(ekf.get_state());
 		t += dt;
 	}
 
 	for (size_t i = 0; i < sim.true_results.size(); i++) {
 		std::cout << dt*i << ", " 
 							<< sim.true_results[i](0) << ", " << sim.true_results[i](1) << ", "
-							<< sim.obs_results[i](0) << ", " << sim.obs_results[i](1)
+							<< sim.obs_results[i](0) << ", " << sim.obs_results[i](1) << ", "
+							<< res[i](0) << ", " << res[i](1) << ", "
 							<< std::endl;
 	}
 
